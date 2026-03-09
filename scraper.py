a4ab4cd069ef35c282f374b0ee7a6d38a7a8e674
bhaditya2317-pixel <bhaditya2317@gmail.com>
2026-03-09 16:55:46 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,1 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
 
+def get_product_data(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
+
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    title = soup.find("span", id="productTitle").text.strip()
+    price = soup.find("span", class_="a-price-whole").text.strip()
+
+    return {
+        "title": title,
+        "price": price
+    }
