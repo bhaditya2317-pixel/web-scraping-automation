e3062e74ba565d075d7915685adc073b2c47c05d
bhaditya2317-pixel <bhaditya2317@gmail.com>
2026-03-09 16:57:42 +0530

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,1 +1,12 @@
+import pandas as pd
+from scraper import get_product_data
 
+URL = "PRODUCT_URL"
+
+product = get_product_data(URL)
+
+df = pd.DataFrame([product])
+
+df.to_csv("data/prices.csv", mode="a", header=False, index=False)
+
+print("Data saved successfully")
